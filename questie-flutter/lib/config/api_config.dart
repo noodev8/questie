dfa439bd4438cbc1070aa8333e3cd96c7730cc13
Summer2906 <Summer.louise2906@gmail.com>
2025-08-21 13:08:38 +0100

    server

--- questie-flutter/lib/config/api_config.dart
+++ questie-flutter/lib/config/api_config.dart
@@ -4,11 +4,11 @@
 class ApiConfig {
   // Base URL options - uncomment the one you want to use
   //static const String baseUrl = 'http://192.168.0.15:3014'; // welshpool - REAL PHONE
-  static const String baseUrl = 'http://192.168.1.187:3014'; // work
+  //static const String baseUrl = 'http://192.168.1.187:3014'; // work
   //static const String baseUrl = 'http://192.168.1.173:3014'; // shrewsbury
   //static const String baseUrl = 'http://localhost:3014'; // localhost
   //static const String baseUrl = 'http://10.0.2.2:3014'; // Android emulator
-  
+  static const String baseUrl = 'https://questie.noodev8.com'; // Android emulator
   // Derived URLs
   static const String apiBaseUrl = '$baseUrl/api';
   static const String authApiUrl = '$apiBaseUrl/auth';
